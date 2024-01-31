280ecf044e019b4d124e3c8b5950a25494d88936
Marco Medrano <marcomedrano1999@gmail.com>
2024-01-31 16:41:24 -0600

    Add ENET lib files

--- Custom_ENET_layer/custom_enet_layer.c
+++ Custom_ENET_layer/custom_enet_layer.c
@@ -8,3 +8,22 @@
 
 #include "custom_enet_layer.h"
 
+/*  SDK Included Files */
+#include "Driver_ETH_MAC.h"
+#include "pin_mux.h"
+#include "board.h"
+#include "fsl_debug_console.h"
+#include "fsl_enet.h"
+#include "fsl_enet_cmsis.h"
+#include "fsl_enet_phy_cmsis.h"
+#include "fsl_phy.h"
+#include "stdlib.h"
+
+#include "fsl_common.h"
+#include "fsl_sysmpu.h"
+#include "fsl_phyksz8081.h"
+#include "fsl_enet_mdio.h"
+#include "RTE_Device.h"
+
+
+
