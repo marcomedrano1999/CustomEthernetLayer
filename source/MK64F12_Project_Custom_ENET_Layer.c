2369c92371b2e1a43c52ba6363c687f42af8a2a1
Marco Medrano <marcomedrano1999@gmail.com>
2024-02-08 09:44:05 -0600

    Add tests

--- source/MK64F12_Project_Custom_ENET_Layer.c
+++ source/MK64F12_Project_Custom_ENET_Layer.c
@@ -23,7 +23,7 @@
 void application_callback(uint8_t *data, uint32_t len)
 {
 	// Print the receive buffer on the console
-	PRINTF(data);
+	PRINTF((char*)data);
 }
 
 
@@ -44,9 +44,37 @@ int main(void) {
     // Initialize the layer
     Custom_ENET_Later_Init(application_callback);
 
-    // Send first package
-    char pck1[] = "Hola 1";
-    Custom_ENET_Later_Transmit(pck1, sizeof(pck1));
+	// Send first package
+	char pck1[] = "01234567890123456789";
+	Custom_ENET_Later_Transmit((uint8_t*)pck1, sizeof(pck1));
+
+	// Send second package
+	char pck2[] = "Hola Mundo";
+	Custom_ENET_Later_Transmit((uint8_t*)pck2, sizeof(pck2));
+
+	// Send third package
+	char pck3[] = "Al que madruga, dios le ayuda";
+	Custom_ENET_Later_Transmit((uint8_t*)pck3, sizeof(pck3));
+
+	// Send fourth package
+	char pck4[] = "Mas vale tarde que nunca";
+	Custom_ENET_Later_Transmit((uint8_t*)pck4, sizeof(pck4));
+
+	// Send fifth package
+	char pck5[] = "Otoño lluvioso, año copioso";
+	Custom_ENET_Later_Transmit((uint8_t*)pck5, sizeof(pck5));
+
+	// Send sixth package
+	char pck6[] = "Mas vale pajaro en mano que ciento volando";
+	Custom_ENET_Later_Transmit((uint8_t*)pck6, sizeof(pck6));
+
+	// Send seventh package
+	char pck7[] = "Me han dicho un dicho, que dicen que he dicho yo. Ese dicho está mal dicho, pues si yo lo hubiera dicho, estaría mejor dicho, que ese dicho que dicen que algún día dije yo.";
+	Custom_ENET_Later_Transmit((uint8_t*)pck7, sizeof(pck7));
+
+	// Send eighth package
+	char pck8[] = "Pablito clavó un clavito en la calva de un calvito. Un clavito clavó Pablito en la calva de un calvito. ¿Qué clavito clavó Pablito?";
+	Custom_ENET_Later_Transmit((uint8_t*)pck8, sizeof(pck8));
 
 
     /* Enter an infinite loop, just incrementing a counter. */
